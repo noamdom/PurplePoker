a9539501d28a85e4a61126f664f1855ea325b2b9
Noam Domovich <ndomovich@gmail.com>
2020-08-23 11:57:13 +0300

    Trying DEBUG -> False

--- RestPoker/settings.py
+++ RestPoker/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q3)3+7oby5#uxj65vf+%_m2&nr8q^j#+lbbtd0^rbex=w5xaxw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['purple-poker.herokuapp.com', '127.0.0.1' ]
-# ALLOWED_HOSTS = [ ]
 
 # Application definition
 
